d92187230375aecdc12d450443765aa19c70dd18
suminpixel <suminpixel@gmail.com>
2021-09-25 17:28:43 +0900

    json pointer test

--- src/modules/ideaboard.cpp
+++ src/modules/ideaboard.cpp
@@ -17,23 +17,17 @@ using namespace std;
 using json = nlohmann::json;
 
 
-// 처음으로 old dom / new dom 의 json tree obj를 stringfy 한 것을 char 로 받음
-// json으로 변환
-// 변환하여 전역에 보관하고 있음
+// (O) 처음으로 old dom / new dom 의 json tree obj를 stringfy 한 것을 pointer 로 받음
+// (O) json으로 변환
+// (O) 변환하여 전역에 보관하고 있음
 // diff 수행
+// root element 포인터화
 // 수행 완료후 전역에 보관하는 dom 제거
 
-#ifdef __EMSCRIPTEN__
-  EMSCRIPTEN_KEEPALIVE
-#endif
-json* old_dom;
+json old_dom;
+json new_dom;
 
-#ifdef __EMSCRIPTEN__
-  EMSCRIPTEN_KEEPALIVE
-#endif
-json* new_dom;
-
-char* my_dom = "{}";
+typedef void(*OnActiveJsFunc)(void);
 
 #ifdef __EMSCRIPTEN__
   EMSCRIPTEN_KEEPALIVE
@@ -54,10 +48,9 @@ void free_buffer(const char* pointer)
 #ifdef __EMSCRIPTEN__
   EMSCRIPTEN_KEEPALIVE
 #endif
-void set_old_dom (char* json_string){
-  my_dom = json_string;
-  //char pointer to string
-  old_dom = json::parse(json_string);
+void init_dom (char* old_pointer, char* new_pointer){ 
+  old_dom = json::parse(old_pointer);
+  new_dom = json::parse(new_pointer);
 }
 
 #ifdef __EMSCRIPTEN__
@@ -65,15 +58,97 @@ void set_old_dom (char* json_string){
 #endif
 void get_old_dom(char* return_json_string)
 {
-  strcpy(return_json_string, my_dom);
+  string s = old_dom.dump();
+  strncpy(return_json_string, s.c_str(), s.size());
 }
 
 #ifdef __EMSCRIPTEN__
   EMSCRIPTEN_KEEPALIVE
 #endif
-int get_number()
+int diff_dom()
 {
-  return 8;
+    // 1. oldNode만 있는 경우 => remove child node
+    if (old_dom.type() != json::value_t::null && new_dom.type() == json::value_t::null) {
+        return 1;
+    }
+
+    // 2. newNode만 있는 경우 => append new child node
+      if (old_dom.type() == json::value_t::null && new_dom.type() != json::value_t::null) {
+        return 2;
+    }
+    
+    // 3. oldNode와 newNode 모두 text 타입일 경우
+    if (old_dom.type() == json::value_t::string && new_dom.type() == json::value_t::string) {
+        if (old_dom == new_dom) return 9; //동일한 값이면 udpate x
+        return 3;
+    }
+
+    // 4. oldNode와 newNode의 태그 이름(type)이 다를 경우
+    if (new_dom["type"] != old_dom["type"]) {
+        return 4;
+    }
+
+    return 0;
+}
+
+#ifdef __EMSCRIPTEN__
+  EMSCRIPTEN_KEEPALIVE
+#endif
+int diff_node(json parent, json old_node, json new_node)
+{
+    // 1. oldNode만 있는 경우 => remove child node
+    if (old_node.type() != json::value_t::null && new_node.type() == json::value_t::null) {
+        return 1;
+    }
+
+    // 2. newNode만 있는 경우 => append new child node
+      if (old_node.type() == json::value_t::null && new_node.type() != json::value_t::null) {
+        return 2;
+    }
+    
+    // 3. oldNode와 newNode 모두 text 타입일 경우
+    if (old_node.type() == json::value_t::string && new_node.type() == json::value_t::string) {
+        if (old_node == new_node) return 9; //동일한 값이면 udpate x
+        return 3;
+    }
+
+    // 4. oldNode와 newNode의 태그 이름(type)이 다를 경우
+    if (old_node["type"] != new_node["type"]) {
+        return 4;
+    }
+
+    return 0;
+}
+
+
+//For Test
+#ifdef __EMSCRIPTEN__
+  EMSCRIPTEN_KEEPALIVE
+#endif
+int diff_all_dom()
+{
+  return (new_dom == old_dom);
+}
+
+//For Test
+#ifdef __EMSCRIPTEN__
+  EMSCRIPTEN_KEEPALIVE
+#endif
+void append_text_to_dom(OnActiveJsFunc ActiveJsFunc)
+{
+    ActiveJsFunc();
+}
+
+
+#ifdef __EMSCRIPTEN__
+  EMSCRIPTEN_KEEPALIVE
+#endif
+void get_dom(char* return_pointer)
+{
+  string o = old_dom.dump();
+  string n = new_dom.dump();
+
+  strncpy(return_pointer, o.c_str(), o.size());
 }
 
 
